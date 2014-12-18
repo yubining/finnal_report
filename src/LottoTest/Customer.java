bd44a847b9e7aaedb52aee06b484070b954e0058
yubin <ybp0118@hanmail.net>
2014-12-19 00:27:46 +0900

    first commit

---
+++ src/LottoTest/Customer.java
@@ -0,0 +1,11 @@
+package LottoTest;
+
+import java.util.*;
+
+public class Customer {
+   public static void main(String[] args) {
+	ServiceCounter CS = new ServiceCounter();
+	Scanner sn = new Scanner(System.in);
+	CS.buyRequest(sn);
+}
+}
