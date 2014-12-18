64bb88dee570288d62daccec7da30573400b5fb2
yubin <ybp0118@hanmail.net>
2014-12-19 02:46:15 +0900

    로또를 판매하는 seller 클래스를 생성, 거기에 필요한 것들을 넣음

--- src/LottoTest/ServiceCounter.java
+++ src/LottoTest/ServiceCounter.java
@@ -14,29 +14,7 @@ public class ServiceCounter {
     }
 
 
-    private boolean isAuto() {
-        // TODO Auto-generated method stub
-        return (lottoselect == 1);
-    }
 
-    private void communication(Scanner sn) {
-        while (true) {
-            System.out.println("자동입력은 1, 직접입력은 2를 눌러주세요");
-            lottoselect = sn.nextInt();
-            try {
-                if (lottoselect == 1) {
-                    System.out.println("자동입력을 선택하였습니다.\n몇장을 사시겠습니까?");
-                    lottoCountSheet = sn.nextInt();
 
-                } else if (lottoselect == 2) {
-                    System.out.println("직접입력을 선택하였습니다.\n몇장을 사시겠습니까?");
-                    lottoCountSheet = sn.nextInt();
-                }
-            } catch (Exception e) {
-            }
-            if (lottoselect == 1 || lottoselect == 2)
-                break;
-            System.out.println("다시입력하세요");
-        }
-    }
+
 }
